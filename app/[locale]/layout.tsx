79f3e2dfb6ab28f822a097f5ac342957da34c3a3
Norbek Qodirov <norbekqodirov@gmail.com>
2026-02-25 16:17:10 +0500

    Make locale routes dynamic for Vercel preview

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,13 +1,7 @@
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages, unstable_setRequestLocale } from "next-intl/server";
 
-import { locales } from "@/i18n/routing";
-
 export const dynamic = "force-dynamic";
-
-export function generateStaticParams() {
-  return locales.map((locale) => ({ locale }));
-}
 
 export default async function LocaleLayout({
   children,
